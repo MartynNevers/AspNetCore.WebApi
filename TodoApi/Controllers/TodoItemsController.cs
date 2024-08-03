591b0fcc2f21d58af351c99bfb53fa59e8af7334
Martyn Nevers <28433993+MartynNevers@users.noreply.github.com>
2024-08-03 02:22:28 +0100

    Set template string in the controller's Route attribute

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -9,7 +9,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
