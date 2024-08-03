abd4ba3190a815a004e8a85f2900288b09c95ddb
Martyn Nevers <28433993+MartynNevers@users.noreply.github.com>
2024-08-03 02:41:05 +0100

    Prevent over-posting

--- TodoApi/Models/TodoItemDTO.cs
+++ TodoApi/Models/TodoItemDTO.cs
@@ -1,9 +1,8 @@
 ﻿namespace TodoApi.Models;
 
-public class TodoItem
+public class TodoItemDTO
 {
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
 }
